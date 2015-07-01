7ad5364baf32c63faedbffd57fa0f1803d8253e6
yuwang <648795318@qq.com>
2015-07-01 21:46:45 +0800

    修改

--- datapicker.js
+++ datapicker.js
@@ -12,13 +12,6 @@
 }(this, function () {
     'use strict';
 
-    //时间常数
-    var _second = 1000,
-        _minute = 60 * _second,
-        _hour = 60 * _minute,
-        _day = 24 * _hour,
-        _week = 7 * _day;
-
     //默认配置项
     var defaults = {
         //绑定的表单元素
@@ -36,7 +29,7 @@
         //初始化时，是否显示值
         setDefaultDate: false,
 
-        //一周的开始星期（0: sunday, 1: monday etc）
+        //一周的开始星期（0: sunday, 1: monday etc.）
         firstDay: 0,
 
         //能选择的最小/最早的日期
@@ -64,15 +57,18 @@
         onDraw: null
     };
 
+
+    //日历构造函数
     function DatePicker (options) {
-        var self = this,
-            opts = self.config(options);
+        options = options || {};
+
+        var self = this;
     }
 
     DatePicker.prototype = {
         //配置函数
         config: function (options) {
-            //var opts =
+            var opts = util.extend()
         },
 
         show: function () {
@@ -92,15 +88,47 @@
     //特征检测及帮助函数
     var util = {
         /**
-         * 对象合并
-         * @param to        {object}    需要合并到的对象
-         * @param from      {object}    合并项
-         * @param overwrite {object}    重写
-         * @returns {*}     {object}    合并后的对象
+         * 检测是否是对象
+         * 需要注意的是，null不被认为是一个对象，array在javascript里被认为是一个对象
          */
-        extend: function (to, from, overwrite) {
+        isObject: function (value) {
+            // http://jsperf.com/isobject4
+            return value !== null && typeof value === 'object';
+        },
+        
+        isFunction: function (value) {
+            return typeof value === 'function';
+        },
 
-            return to;
+        /**
+         * 合并对象，将源对象的所有属性复制到目标对象上
+         * @param destination   {object}    目标对象
+         * @param source        {object}    [可选]源对象
+         * @param deep          {boolean}   [可选]是否复制(继承)对象中的对象，默认：false
+         * @returns {object}                返回继承了source对象属性的新对象
+         */
+        extend: function (destination, source, deep) {
+
+            for(var i = 0, j = source.length; i < j; ++i) {
+                var obj = source[i];
+
+                if (!util.isObject(obj) && !util.isFunction(obj)) continue;
+                var keys = Object.keys(obj);
+
+                for(var m = 0, n = keys.length; m < n; ++m) {
+                    var key = keys[i], src = obj[key];
+
+                    if(deep && util.isObject(src)) {
+                        if(!util.isObject(destination[key])) {
+                            destination[key] = isArray(src) ? [] : {};
+                        }
+                        util.extend(destination[key], [src], true);
+                    }
+                }
+            }
+
+
+            return destination;
         }
     };
 
