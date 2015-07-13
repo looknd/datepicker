17024b8bf48ea363ea8015a20d7ed2e05de0a498
yuwang <648795318@qq.com>
2015-07-13 18:31:16 +0800

    修改合并对象函数

--- datapicker.js
+++ datapicker.js
@@ -1,9 +1,9 @@
 (function (root, factory) {
-    if(typeof exports === 'object') {
-        //CommonJs
+    if (typeof exports === 'object') {
+        // CommonJs
         module.exports = factory();
-    } else if(typeof define === 'function' && define.amd) {
-        //AMD
+    } else if (typeof define === 'function' && define.amd) {
+        // AMD
         define([], factory);
     } else {
         // Browser globals (root is window)
@@ -12,45 +12,37 @@
 }(this, function () {
     'use strict';
 
-    //默认配置项
+    // 默认配置项
     var defaults = {
-        //绑定的表单元素id
+        // 绑定的表单元素id
         id: null,
 
-        //获取焦点时，是否自动显示picker
-        bound: undefined,
-
-        //格式化日期
+        // 格式化日期
         format: 'YYYY-MM-DD',
 
-        //初始化的值
+        // 初始化的值
         defaultDate: null,
 
-        //初始化时，是否显示值
+        // 初始化时，是否显示值
         setDefaultDate: false,
 
-        //一周的开始星期（0: sunday, 1: monday etc.）
+        // 一周的开始星期（0: sunday, 1: monday etc.）
         firstDay: 0,
 
-        //能选择的最小/最早的日期
+        // 能选择的最小/最早的日期
         minDate: null,
 
-        //能选择的最大/最迟的日期
+        // 能选择的最大/最迟的日期
         maxDate: null,
 
-        //年份的范围，或数组的 上限/下限 范围
-        yearRange: 10,
-
-        //internationalization
+        // internationalization
         i18n: {
-            previousMonth: 'Previous Month',
-            nextMonth: 'Next Month',
             month: ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'],
             weekdays: ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'],
             weekdaysShot: ['日', '一', '二', '三', '四', '五', '六']
         },
 
-        //回调函数
+        // 回调函数
         onSelect: null,
         onOpen: null,
         onClose: null,
@@ -58,65 +50,70 @@
     };
 
 
-    //日历构造函数
-    function DatePicker (options) {
-        var self = this;
+    // 日历构造函数
+    function DatePicker(options) {
+
     }
 
+    // 对外开放的API
     DatePicker.prototype = {
-        //配置函数
+        // 配置函数
         config: function (options) {
-            if(!this._o) {
+            if (!this._o) {
                 this._o = util.extend({}, defaults);
             }
             var opts = util.extend(this._o, options, true);
 
-            //处理id是否存在
+            // 处理id是否存在
             var field = document.getElementById(opts.id);
             this.field = (field && field.nodeName) ? field : null;
-
-            //处理自动显示
-
-        },
-
-        renderHeader: function () {
-            return ''
         }
     };
 
 
-    //特征检测及帮助函数
-    var util = {
-        /**
-         * 检测是否是对象
-         * 需要注意的是，此函数里null不被认为是一个对象，array在javascript里被认为是一个对象
-         */
-        isObject: function (value) {
-            // http://jsperf.com/isobject4
-            return value !== null && typeof value === 'object';
-        },
-        
-        extend: function (to, from, overwrite) {
-            overwrite = overwrite || false;
-            var prop, hasProp;
+    function renderHeader() {
+        var pickerHeaderHtml = '';
+        pickerHeaderHtml += '<select class="calendar-select-year">';
+        pickerHeaderHtml += '<option value="0">2015</option>';
+        pickerHeaderHtml += '</select>';
+    }
 
-            for(prop in from) {
-                hasProp = to[prop] !== undefined;
+    function getMonth() {
 
-                if(hasProp && util.isObject(from[prop])) {
-                    //存在相同属性，需要覆盖合并时，且form的属性为object, 递归合并
-                    if(overwrite) {
-                        util.extend(to[prop], from[prop], overwrite);
-                    }
-                } else if(!hasProp || overwrite) {
-                    //不存在相同属性或需要覆盖合并时, 将此属性添加到 to 里
-                    to[prop] = from[prop];
-                }
+    }
+
+    function getYear(year) {
+
+    }
+
+
+    // 检测是否是对象
+    function isObject(value) {
+        // http://jsperf.com/isobject4
+        return value !== null && typeof value === 'object';
+    }
+
+
+    //合并对象
+    function extend(to, from, overwrite) {
+        overwrite = overwrite || false;
+        var prop, hasProp;
+
+        for (prop in from) {
+            hasProp = (to[prop] !== undefined);
+
+            if(hasProp && isObject(from[prop])) {
+                //拥有相同属性，属性值为object，执行递归合并
+                extend(to[prop], from[prop], overwrite);
+            } else if (!hasProp || overwrite) {
+                //不具有相同属性时，将不相同的属性值添加到目标对象上去
+                //需要覆盖合并时，直接覆盖目标对象的所有属性
+                to[prop] = from[prop];
             }
-
-            return to;
         }
-    };
+
+        return to;
+    }
 
     return DatePicker;
 }));
